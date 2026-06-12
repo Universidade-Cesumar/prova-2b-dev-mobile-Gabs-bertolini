02d790a8a7819386f1eccc6d4a36bb0e936ae9ee
Gabs-bertolini <gabrielbertolinibarioni@gmail.com>
2026-06-11 21:45:00 -0300

    configure jest and update dependencies

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { registerRootComponent } from 'expo';
+
 import App from './App';
 
-// Esse comando força o Expo a registrar o seu App.js como a raiz do projeto, tanto para Mobile quanto para Web
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
 registerRootComponent(App);
